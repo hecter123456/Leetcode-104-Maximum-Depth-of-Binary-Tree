eedbbe7d6d8b4f3ce01466c3153dc1f668968139
hecter <hecter654321@gmail.com>
2018-02-24 08:12:38 +0800

    pass testcase: testSample by using DFS

--- Maximum Depth of Binary Tree.py
+++ Maximum Depth of Binary Tree.py
@@ -1,4 +1,5 @@
 import unittest
+import TreeSolution
 
 class TreeNode(object):
     def __init__(self, x = None):
@@ -11,11 +12,18 @@ class unitest(unittest.TestCase):
         Input = None
         Output = 0
         self.assertEqual(Solution().maxDepth(Input),Output);
+    def testSample(self):
+        row = [3,9,20,None,None,15,7]
+        tree = TreeSolution.TreeSolution()
+        Input = tree.AddBinaryTreeNode(row)
+        Output = 3
+        self.assertEqual(Solution().maxDepth(Input),Output);
 
 class Solution():
     def maxDepth(self, root):
         if root == None:
-            return 0 
+            return 0
+        return max(self.maxDepth(root.left),self.maxDepth(root.right)) + 1
 
 if __name__ == '__main__':
     unittest.main()
